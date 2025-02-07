5febe338192225e2b3ad8d5875f8d1f035b28dca
foysal767 <uddinmohammadfoysal981@gmail.com>
2025-02-07 23:27:14 +0600

    another page added

--- src/Pages/CourseDetails/CourseDetails.jsx
+++ src/Pages/CourseDetails/CourseDetails.jsx
@@ -3,7 +3,8 @@ const CourseDetails = () => {
     return (
         <div>
             <h1>Course Details Page</h1>
-            <h2>Course Details Page</h2>
+            <h2>Course Details Page2</h2>
+            <h2>Course Details Page3</h2>
         </div>
     );
 };
