d82a4824c6c47f92cb641c8245f3769007ede7da
foysal767 <uddinmohammadfoysal981@gmail.com>
2025-02-07 23:30:16 +0600

    another setting updated

--- src/Pages/CourseDetails/CourseDetails.jsx
+++ src/Pages/CourseDetails/CourseDetails.jsx
@@ -5,6 +5,7 @@ const CourseDetails = () => {
             <h1>Course Details Page</h1>
             <h2>Course Details Page2</h2>
             <h2>Course Details Page3</h2>
+            <h3>Course Details Page3</h3>
         </div>
     );
 };
