80fa1db83e8ddcd564b542ce684a0bcf17e9fddd
sumaiya ahmed <sumu0612@gmail.com>
2025-02-09 21:56:34 +0600

    tailwind version 4 updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import useEduCourseContexts from "../../Hooks/UseEduCourseContexts";
 const Home = () => {
 const {name} = useEduCourseContexts()
     return (
-        <div className="border">
+        <div className="border text-5xl text-red-500">
             <h1>Home Page</h1>
             <p>Name: {name}</p>
         </div>
