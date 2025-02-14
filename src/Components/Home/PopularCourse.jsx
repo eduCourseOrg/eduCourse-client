dffe441059ce6bf0dda043eab2be5bcdb8cf0b52
arifulislam38 <mdarifulislam0238@gmail.com>
2025-02-14 09:40:22 +0600

    popular courses updated with a slide

--- src/Components/Home/PopularCourse.jsx
+++ src/Components/Home/PopularCourse.jsx
@@ -3,8 +3,26 @@ import courseBanner1 from '/images/course-banner/course-banner-3.jpg'
 import { FaClock, FaStar } from "react-icons/fa";
 import { PiCellSignalFullBold } from "react-icons/pi";
 import { TbWorld } from "react-icons/tb";
+import { useQuery } from '@tanstack/react-query';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const PopularCourse = () => {
+    const {data: courses = [], isLoading } = useQuery({
+        queryKey: ['course'],
+        queryFn: async() => {
+            const res = await fetch('http://localhost:3000/courses')
+            const data = await res.json()
+            return data;
+        }
+
+    })
+
+    if (isLoading) {
+        return <div className="w-full h-[100vh] flex items-center justify-center"><h1 className="text-5xl">Loading....</h1></div>
+    }
     return (
         <div className="w-full h-[100vh] bg-secondary p-4 flex flex-col gap-3">
             <div className="w-full flex items-center justify-between">
@@ -22,15 +40,22 @@ const PopularCourse = () => {
             </div>
 
             <div className="w-full h-[86%] flex gap-4">
-                {/* Card One start form here */}
-                <div className="w-full h-full bg-white">
+                <Swiper
+                    modules={[Pagination]}
+                    slidesPerView={3}
+                    grabCursor={true}
+                    spaceBetween={30}
+                    pagination={{clickable: true}} 
+                >
+                    {
+                    courses.map((course, idx) => <SwiperSlide key={idx} className="w-full h-full bg-white">
                     {/* Card Image Section */}
                     <div className="w-full h-[45%] mb-3">
                         <img src={courseBanner1} alt="" className="w-full h-full" />
                     </div>
                     {/* Card Content Section */}
                     <div className="w-full h-[55%] px-3 flex flex-col gap-1">
-                        <h1 className="text-2xl font-bold">The complete Digital Marketing Bootcamp</h1>
+                        <h1 className="text-2xl font-bold">{course.name}</h1>
                         <div className="flex items-center justify-between">
                             <span className="text-sm font-semibold">By: Md. Foysal Uddin</span>
                             <h2 className="text-xl font-bold">$99</h2>
@@ -53,72 +78,12 @@ const PopularCourse = () => {
                             <span className="flex items-center gap-1"><TbWorld></TbWorld> English</span>
                         </div>
                 </div>
-                </div>
-                {/* Card Two start form here */}
-                <div className="w-full h-full bg-white">
-                    {/* Card Image Section */}
-                    <div className="w-full h-[45%] mb-3">
-                        <img src={courseBanner1} alt="" className="w-full h-full" />
-                    </div>
-                    {/* Card Content Section */}
-                    <div className="w-full h-[55%] px-3 flex flex-col gap-1">
-                        <h1 className="text-2xl font-bold">The complete Digital Marketing Bootcamp</h1>
-                        <div className="flex items-center justify-between">
-                            <span className="text-sm font-semibold">By: Md. Foysal Uddin</span>
-                            <h2 className="text-xl font-bold">$99</h2>
-                        </div>
-                        <div>
-                            <h2 className="flex items-center">
-                                <FaStar className="text-yellow-400"></FaStar>
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                (2)
-                            </h2>
-                        </div>
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, modi.</h3>
-                        <hr className="bg-primary h-[2px] mt-5"/>
-                        <div className="mt-2 flex items-center justify-between font-semibold">
-                            <span className="flex items-center gap-1"><FaClock></FaClock> 3 Days</span>
-                            <span className="flex items-center gap-1"><PiCellSignalFullBold></PiCellSignalFullBold> Beginner</span>
-                            <span className="flex items-center gap-1"><TbWorld></TbWorld> English</span>
-                        </div>
-                </div>
-                </div>
-                {/* Card Three start form here */}
-                <div className="w-full h-full bg-white">
-                    {/* Card Image Section */}
-                    <div className="w-full h-[45%] mb-3">
-                        <img src={courseBanner1} alt="" className="w-full h-full" />
-                    </div>
-                    {/* Card Content Section */}
-                    <div className="w-full h-[55%] px-3 flex flex-col gap-1">
-                        <h1 className="text-2xl font-bold">The complete Digital Marketing Bootcamp</h1>
-                        <div className="flex items-center justify-between">
-                            <span className="text-sm font-semibold">By: Md. Foysal Uddin</span>
-                            <h2 className="text-xl font-bold">$99</h2>
-                        </div>
-                        <div>
-                            <h2 className="flex items-center">
-                                <FaStar className="text-yellow-400"></FaStar>
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                <FaStar className="text-yellow-400"></FaStar> 
-                                (2)
-                            </h2>
-                        </div>
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, modi.</h3>
-                        <hr className="bg-primary h-[2px] mt-5"/>
-                        <div className="mt-2 flex items-center justify-between font-semibold">
-                            <span className="flex items-center gap-1"><FaClock></FaClock> 3 Days</span>
-                            <span className="flex items-center gap-1"><PiCellSignalFullBold></PiCellSignalFullBold> Beginner</span>
-                            <span className="flex items-center gap-1"><TbWorld></TbWorld> English</span>
-                        </div>
-                </div>
-                </div>
+                </SwiperSlide>)
+                }
+                </Swiper>
+                
             </div>
+            
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,16 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Router/Router';
 import AuthProvider from './Contexts/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient(); 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}> 
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
